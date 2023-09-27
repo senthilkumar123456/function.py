a7f16e8865f6776381ced3f8d204955eab592160
senthilkumar123456 <141622183+senthilkumar123456@users.noreply.github.com>
2023-09-27 11:59:59 +0530

    function.py

---
+++ function.py
@@ -0,0 +1,52 @@
+##def myfunction():
+##    string=str(input('enter the character'))
+##    count=len(string)
+##    print('number of character',count)
+##myfunction()
+
+##def addition():
+##    a=3
+##    b=7
+##    print(a+b)
+##addition
+
+##def mypop1():
+##    set1={10,'k','i','sebh'}
+##    set1.pop()
+##    print(set1)
+##mypop1()
+
+##def mylist1():
+##    list1=[1,2,3]
+##    list2=[7,8]
+##    if (list1[0]==list2[-2]):
+##        print('True')
+##    else:
+##        print('false')
+##mylist1()
+
+##def mybreak():
+##    num=[(10,20),30,40,50,60]
+##    ctr=0
+##    for i in num:
+##        if isinstance(i,tuple):
+##            break
+##        ctr+=1
+##    print(ctr)
+##mybreak()
+
+##def myrange():
+##    rows=int(input('enter the rows values'))
+##    for i in range(rows,0,-1):
+##        for j in range(0,i,1):
+##            print('*',end=' ')
+##        print()
+##myrange()           
+             
+
+
+
+
+
+
+
